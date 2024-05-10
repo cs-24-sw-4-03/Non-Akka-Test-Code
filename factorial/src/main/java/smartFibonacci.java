0ad59ef2e2099d785ec611405e2263b421d93118
Laczse <lass738a@ucholstebro.dk>
2024-05-10 14:04:22 +0200

    Created standard and smart sequential implementation of Fibonacci

---
+++ factorial/src/main/java/smartFibonacci.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+
+public class smartFibonacci{
+
+    static long[] longArray;
+    public static void main(String[] args) {
+        longArray = new long[101];
+        longArray[0] = 0;
+        longArray[1] = 1;
+        System.out.println(fibonacci(100));
+    }
+
+    private static long fibonacci(int n) {
+        if (n == 0){
+            return n;
+        }else if (longArray[n] == 0){
+            longArray[n] = fibonacci(n-1) + fibonacci(n-2);
+            return longArray[n];
+        }else{
+            return longArray[n];
+        }
+    }
+}
---
+++ factorial/src/main/java/standardFibonacci.java
@@ -0,0 +1,11 @@
+public class standardFibonacci {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(50));
+    }
+
+    private static long fibonacci(long n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
